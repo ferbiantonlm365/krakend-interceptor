fd5fc2349a00ab0156a0aa5328c1492a77f6339f
Ferbianto Andri <vintorezsr@gmail.com>
2020-12-30 02:00:53 +0700

    Update plugin

--- proxy-plugin/plugin.go
+++ proxy-plugin/plugin.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"html"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -21,28 +22,60 @@ func (r registerer) RegisterClients(f func(
 }
 
 func (r registerer) registerClients(ctx context.Context, extra map[string]interface{}) (http.Handler, error) {
-	// check the passed configuration and initialize the plugin
 	name, ok := extra["name"].(string)
 
 	if !ok {
-		return nil, errors.New("wrong config")
+		return nil, errors.New("Name must be defined")
 	}
 
 	permissions, ok := extra["permissions"].(string)
 
 	if !ok {
-		return nil, errors.New("wrong config")
+		return nil, errors.New("Permissions must be defined")
 	}
 
-	fmt.Println("permissions: ", permissions)
+	endpoint, ok := extra["endpoint"].(string)
+
+	if !ok {
+		return nil, errors.New("Endpoint must be defined")
+	}
 
 	if name != string(r) {
 		return nil, fmt.Errorf("unknown register %s", name)
 	}
-	// return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http client
+
+	// Return the actual handler wrapping or your custom logic so it can be used as a replacement for the default http client
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		fmt.Println("proxy-plugin called asshole")
-		fmt.Println("permissions: ", permissions)
+		// Create new HTTP client object.
+		client := &http.Client{}
+
+		// Create new HTTP POST request object.
+		newReq, err := http.NewRequest(http.MethodPost, endpoint, nil)
+		newReq.Header.Set("X-Permission", permissions)
+
+		// Copy source header to destination header.
+		for k, vv := range req.Header {
+			for _, v := range vv {
+				newReq.Header.Add(k, v)
+			}
+		}
+
+		resp, err := client.Do(newReq)
+
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+		}
+
+		if resp.StatusCode >= http.StatusBadRequest {
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+			bodyMessage := string(bodyBytes)
+
+			http.Error(w, bodyMessage, resp.StatusCode)
+
+			return
+		}
+
+		fmt.Println("proxy-plugin called")
 		fmt.Fprintf(w, "[{\"message\": \"Hello, %s\"}]", html.EscapeString(req.URL.Path))
 	}), nil
 }
